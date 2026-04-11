67e7d8377d06445cc2f71e45b978f56cccf5e017
iringire12 <iringireirihoh@gmail.com>
2026-04-11 11:31:52 +0200

    new

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const clientUrl = process.env.CLIENT_URL || 'http://localhost:3000';
+const clientUrl = process.env.CLIENT_URL || 'https://nurox-company-xqn6.vercel.app';
 
 app.use(
   cors({
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = async () => {
-  const mongoUri = process.env.MONGODB_URI;
+  const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://IRINGIRE:hubert2007@cluster0.0xdxxax.mongodb.net/nurox_auth?majority=true&w=majority';
 
   if (!mongoUri) {
     throw new Error('MONGODB_URI is missing from the environment');
